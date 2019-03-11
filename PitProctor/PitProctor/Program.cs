c399899a8ad66fc948643b1792abcf2eb6eb4667
Prava Chaitanya <chaitanya.prava@dot.state.fl.us>
2019-03-11 15:20:57 -0400

    Finished the following tasks 1. Setup the project 'PitProctor' 2. Created the Testing Project. 3. Setup the '.gitignore'

---
+++ PitProctor/PitProctor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PitProctor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Master Branch");
+        }
+    }
+}
